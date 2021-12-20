7b3be4ad24d52b457e7cfda8d68d0e9a74ac80c6
Bianca <biancasouzarocha@outlook.com>
2021-12-20 13:47:58 -0300

    Adicionando método findAllByName

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
 	public Optional <Usuario> findByUsuario (String Usuario);
 	
 	public List <Usuario> findAllByNomeContainingIgnoreCase (String nome);
